6e3cbc03f2ee93931f21b246b09f90e4389b555f
Jeffrey A. Clark <aclark@aclark.net>
2024-10-16 17:25:53 -0400

    Update polls

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django_mongodb.manager import MongoManager 
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    objects = MongoManager()
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    objects = MongoManager()
 
