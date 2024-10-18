b7f151e0662183d48727b9f3a2fd1f283746168d
Jeffrey A. Clark <aclark@aclark.net>
2024-10-18 17:46:12 -0400

    Fix polls

--- polls/admin.py
+++ polls/admin.py
@@ -4,9 +4,9 @@ from .models import Choice, Question
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ("choice_text", "question", "votes")
+    """Choice Admin"""
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("question_text", "pub_date", "was_published_recently")
+    """Question Admin"""
